2dc1c7b6579f12f7032dd7fcc738fcaec55dba6c
Sam Grayson <sam@samgrayson.me>
2018-03-23 00:55:19 -0500

    added initial animations

--- animation.py
+++ animation.py
@@ -1,17 +1,12 @@
+#!/usr/bin/env python3
 import itertools
 import colorsys
 import neopixel
 import time
 import primefac
 import math
+from math import sin, cos, pi
 import random
-
-
-strip = neopixel.Adafruit_NeoPixel(
-    num=53, # number of pixels
-    pin=18, # GPIO pin connected to the pixels (18 uses PWM!).
-    strip_type=neopixel.ws.WS2812_STRIP,
-)
 
 
 black = neopixel.Color(0, 0, 0)
@@ -27,16 +22,17 @@ def rgb_to_irgb(rgb):
 
 
 def color_wheel(hue):
-    '''hue on a scale from zero to 1'''
+    '''hue on a scale from 0 to 1'''
     return rgb_to_irgb(colorsys.hsv_to_rgb(hue, 1, 1))
 
 
 def mapp(val, inmin, inmax, outmin, outmax):
+    '''maps val on a scale from inmin to inmax to a value on a scale from outmin to outmax'''
     return (val - inmin) / (inmax - inmin) * (outmax - outmin) + outmin
 
 
 def color_val(val):
-    '''val in a scale -1 1'''
+    '''visualizes value val in a scale -1 1 on a scale between red and blue'''
     n = 3
     rgb = (
         val**n    if val > 0 else 0,
@@ -71,10 +67,14 @@ def animate_sequence(strip, sequence, n_colors=200, delay=0.02, n_set_before_off
 
 
 def to_binary(n, size):
+    '''return an integer in binary as a string'''
     return '{n:0{size}b}'.format(**locals())
 
 
 def animate_timestamp(strip, delay=0.0001):
+    '''Display the number of miliseconds since Jan 1, 1970' as a binary number
+
+    (AKA binary counter)'''
     while True:
         timestamp = int(time.time() * 1000)
         for pixel, digit in enumerate(to_binary(timestamp, strip.numPixels())):
@@ -86,13 +86,12 @@ def animate_timestamp(strip, delay=0.0001):
 
 
 def animate_factorial(strip):
+    '''Display n! in binary'''
     delay = 0.5
     acc = 2
     while True:
         for i in itertools.count(2):
             acc *= i
-
-
             for pixel, digit in enumerate(to_binary(acc, strip.numPixels())):
                 color = color_wheel(pixel / strip.numPixels()) if digit == '1' else black
                 strip.setPixelColor(pixel, color)
@@ -114,7 +113,7 @@ def reflect(pos, size, onbounce=None):
 
 def draw_wave(pixel_buffer, wave_spec):
     for pixel in range(round(wave_spec['pos']), round(wave_spec['pos'] + wave_spec['wavelength'])):
-        val = math.sin((pixel - wave_spec['pos']) * 2 * math.pi / wave_spec['wavelength']) * wave_spec['amp']
+        val = sin((pixel - wave_spec['pos']) * 2 * math.pi / wave_spec['wavelength']) * wave_spec['amp']
         pixel_buffer[reflect(pixel, len(pixel_buffer))] += val
 
 
@@ -153,7 +152,7 @@ def animate_waves(strip, wave_specs=None):
         time.sleep(delay)
 
 
-def animate_icicle(strip, prob_water=0.001, drip_speed=20, max_splash_height=6, splash_speed=6, delay=0.0001):
+def animate_icicle(strip, prob_water=0.001, drip_speed=20, max_splash_height=5, splash_speed=4, delay=0.0001):
     # note that speed is really 'inverse speed', number of frames between things
     ice_color   = neopixel.Color(200, 200, 255)
     water_color = neopixel.Color(40, 40, 255)
@@ -164,6 +163,7 @@ def animate_icicle(strip, prob_water=0.001, drip_speed=20, max_splash_height=6, 
     splishes = []
     splash_height = 0
     icicle_length = random.randint(14, 17)
+    drip_speed_ = drip_speed
 
     for frame in itertools.count():
         for i in range(icicle_length):
@@ -177,7 +177,7 @@ def animate_icicle(strip, prob_water=0.001, drip_speed=20, max_splash_height=6, 
             for splish in splishes:
                 strip.setPixelColor(splish, water_color)
 
-        if drip_falling and frame % drip_speed == 0:
+        if drip_falling and frame % round(drip_speed_) == 0:
             if drip_pos == 0:
                 print('splash')
                 drip_falling = False
@@ -187,6 +187,7 @@ def animate_icicle(strip, prob_water=0.001, drip_speed=20, max_splash_height=6, 
             else:
                 strip.setPixelColor(drip_pos, black)
                 drip_pos -= 1
+                drip_speed_ -= 0.4
 
         if splash_rising and frame % splash_speed == 0:
             if splash_pos > splash_height:
@@ -197,13 +198,14 @@ def animate_icicle(strip, prob_water=0.001, drip_speed=20, max_splash_height=6, 
             else:
                 strip.setPixelColor(splash_pos, black)
                 for i in range(0, splash_pos):
-                    if random.random() < 0.05:
+                    if random.random() < 0.01:
                         splishes.append(splash_pos)
                 splash_pos += 1
 
         if random.random() < prob_water and not drip_falling:
             print('drip')
             drip_pos = strip.numPixels() - icicle_length - 1
+            drip_speed_ = drip_speed
             drip_falling = True
             if random.random() < prob_water:
                 print('melt')
@@ -216,13 +218,56 @@ def animate_icicle(strip, prob_water=0.001, drip_speed=20, max_splash_height=6, 
         strip.show()
         time.sleep(delay)
 
+def color_wheel2(time):
+    section = math.floor(time*6) # an integer from 0 to 5
+    up = time*6 % 1 # float 0 to 1, increasing with time
+    slowup = up**9
+    down = 1-up # float from 1 to 0
+    slowdown = down**9
+    red   = [       1,     down,   slowup, slowdown,       up,        1]
+    green = [  slowup, slowdown,       up,        1,        1,     down]
+    blue  = [      up,        1,        1,     down,   slowup, slowdown]
+    return rgb_to_irgb((red[section], green[section], blue[section]))
+
+def animate_rainbow(strip):
+    speed_base = 1e-1
+    speed_amp = 7e-2
+    speed = 1 / 1100
+
+    N = strip.numPixels()
+    rainbow_center = 0
+
+    def f_rainbow_center(x):
+        # return 4*sin(2*pi*x) + 2*sin(6*pi*x) - sin(4*pi*x)
+        return sin(2*pi*x) + sin(6*pi*x) - sin(4*pi*x)
+
+    # these will be used to normalize f_rainbow_center
+    max_c = max(f_rainbow_center(x/1000) for x in range(0, 1000))
+    min_c = min(f_rainbow_center(x/1000) for x in range(0, 1000))
+
+    for t in itertools.count():
+        # normalize f_rainbow_center from its range to the LED range
+        rainbow_center = mapp(f_rainbow_center(t * speed % 1), min_c, max_c, 0, N)
+
+        for pixel_loc in range(strip.numPixels()):
+            rainbow_loc = ((pixel_loc - rainbow_center) + N) % N
+            color = color_wheel2(rainbow_loc / N)
+            strip.setPixelColor(pixel_loc, color)
+
+        strip.show()
+
+# TODO: particle collision
 
 
-# particle collision
+strip = neopixel.Adafruit_NeoPixel(
+    num=53, # number of pixels
+    pin=18, # GPIO pin connected to the pixels (18 uses PWM!).
+    strip_type=neopixel.ws.WS2812_STRIP,
+)
 
 strip.begin()
 try:
-    animate_icicle(strip)
+    animate_rainbow(strip)
 finally:
     for pixel in range(strip.numPixels()):
         strip.setPixelColor(pixel, black)
